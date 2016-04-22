b52ffa07aad6dd4e63d7c928ce8214521db65fd2
Michael Mead <michaelmead007@icloud.com>
2016-04-22 12:00:32 -0500

    Challenge Complete.

--- main.js
+++ main.js
@@ -6,15 +6,22 @@
 
 var integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
 
-//sort descending
-integers.sort(function (a,b) {return b-a});
-console.log("sort:", integers);
-//remove integers greater than 19
-integers.splice(0, 4);
-console.log("splice: ", integers);
-// multiply remaining numbers 
-integers = integers.map(function(num){return num * 1.5 -1});
-console.log("multiply: ", integers);
-//sum all remaining numbers.
+// //sort descending
+// integers.sort(function (a,b) {return b-a});
+// console.log("sort:", integers);
+// //remove integers greater than 19
+// integers.splice(0, 4);
+// console.log("splice: ", integers);
+// // multiply remaining numbers 
+// integers = integers.map(function(num){return num * 1.5 -1});
+// console.log("multiply: ", integers);
+// //sum all remaining numbers.
+// integers = integers.reduce(function(previousValue, currentValue, index, array){return previousValue + currentValue});
+// console.log("sum", integers);
 
-console.log("sum", integers);
+//add all methods together.
+integers = integers
+.sort(function (a,b) {return b-a})
+.splice(0, 4).map(function (num){return num *1.5 -1})
+.reduce(function (previousValue, currentValue, index, array){return previousValue + currentValue});
+console.log("Single line answer: ", integers);
